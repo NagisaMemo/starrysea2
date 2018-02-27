fae3e264d32ac31ff9cb267007bac0a4ee09e4e8
xiongjunlang <xiongjunlang@agree.com.cn>
2018-02-27 10:38:50 +0800

    增加eureka服务器以及各个服务的客户端配置

--- top.starrysea.eureka.server/src/main/java/top/starrysea/eureka/server/Application.java
+++ top.starrysea.eureka.server/src/main/java/top/starrysea/eureka/server/Application.java
@@ -1,10 +1,10 @@
-package top.starrysea.activity;
+package top.starrysea.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
+@EnableEurekaServer
 @SpringBootApplication
 public class Application {
 
